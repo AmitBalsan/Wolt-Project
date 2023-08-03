45e41b4ef1dcd475bfadbe0ef7fd773e3a1592a7
iidan99 <iidan99@hotmail.co.il>
2023-08-04 00:39:33 +0300

    idan - add code

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,2 +1,6 @@
-function handleSignUp() { }
+function handleSignUp() {
+    var loginPopup = document.querySelector(".login-panel");
+    console.log(loginPopup);
+    loginPopup.innerHTML = "<div>\n      <form>\n      <label>First Name</label>\n      <input type=\"text\" name=\"first-name\">\n\n      </form>\n      </div>\n      ";
+}
 function handleLogin() { }
--- public/index.ts
+++ public/index.ts
@@ -1,3 +1,15 @@
-function handleSignUp() {}
+function handleSignUp() {
+  const loginPopup = document.querySelector(".login-panel") as HTMLDivElement;
+  console.log(loginPopup);
+
+  loginPopup.innerHTML = `<div>
+      <form>
+      <label>First Name</label>
+      <input type="text" name="first-name">
+
+      </form>
+      </div>
+      `;
+}
 
 function handleLogin() {}
